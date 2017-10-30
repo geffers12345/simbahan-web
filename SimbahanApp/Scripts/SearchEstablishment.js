ecd2ac2527ec291ff391f5e5c1fed2470eb0a53a
ejaydy <erwin.j.dy@gmail.com>
2017-10-30 12:46:56 +0700

    Suggested search place

--- SimbahanApp/Scripts/SearchEstablishment.js
+++ SimbahanApp/Scripts/SearchEstablishment.js
@@ -7,6 +7,8 @@ function initMap() {
     };
 
     var input = document.getElementById('searchEstablishment');
+    var input2 = document.getElementById('startPosition');
 
     var autocomplete = new google.maps.places.Autocomplete(input, options);
+    var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 }
